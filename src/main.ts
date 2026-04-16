2faeb0885e7dc060e4817fc82dd638c891a4adca
BiiaBraga <beatrizsagrado9@gmail.com>
2026-04-16 10:52:23 -0300

    feat: config aplicação e banco de dados

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+//importações
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
+//função de inicialização
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  process.env.TZ = '-03:00';  // configuração do timezone
+  app.useGlobalPipes(new ValidationPipe);  //configuração de validação de dados de entrada
+  app.enableCors();  //configuração de cors para permitir requisições de outras origens
+  await app.listen(process.env.PORT ?? 3000);  //execução da aplicação nest, configuração da porta do servidor
 }
 bootstrap();
